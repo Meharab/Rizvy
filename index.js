0bb52773b3afdd59d35d938532d19af6a5f82cfe
Meharab G <33731973+Meharab@users.noreply.github.com>
2025-05-31 22:16:33 +0600

    added local connection to index.js

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ app.use(cors());
 
 const abi = JSON.parse(fs.readFileSync('./MyTokenABI.json')).abi;
 
+const localProvider = new ethers.JsonRpcProvider(process.env.LOCAL_RPC_URL);
 const sepoliaProvider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
 
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
 
+const localSigner = wallet.connect(localProvider);
 const sepoliaSigner = wallet.connect(sepoliaProvider);
 
+const localContract = new ethers.Contract(process.env.CONTRACT_ADDRESS_LOCAL, abi, localSigner);
 const sepoliaContract = new ethers.Contract(process.env.CONTRACT_ADDRESS_SEPOLIA, abi, sepoliaSigner);
 
 function getContract(chain) {
-  return chain === 'sepolia' ? sepoliaContract : console.log("Supported contract not found");
+  return chain === 'sepolia' ? sepoliaContract : localContract;
 }
 
 app.post('/mint', async (req, res) => {
